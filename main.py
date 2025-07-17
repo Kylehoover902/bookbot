0a22c30f2b0c5542efbff176d19c14b309957cd8
kylehoover902 <kylehoover902@gmail.com>
2025-07-16 21:28:12 -0400

    get_character_count Added

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
     book = "books/frankenstein.txt"
     book_text = get_book_text(book)
+    
     from stats import get_word_count
+    from stats import get_character_count
     print(f"{get_word_count(book_text)} words found in the document")
+    # print(get_character_count(book_text))
+    test = {}
+    print(get_character_count(book_text))
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
+    
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,14 @@ def get_word_count(text):
     for word in words:
         count += 1
     return count
+
+def get_character_count(text):
+    text = text.lower()
+    characters = {}
+    char_list = list(text)
+    for char in char_list:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+    return characters
